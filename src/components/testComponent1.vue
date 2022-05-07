510a6105801de8ef3d083eb3ab246204a91fb3ee
MRSTK90 <dkgell13@gmail.com>
2022-05-07 21:59:28 +0900

    create : require Test 파일 생성

---
+++ src/components/testComponent1.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'testComponent1'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/testComponent2.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'testComponent2'
+}
+</script>
+
+<style scoped>
+
+</style>
